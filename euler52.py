8824a47706f5264691c87f21d20e2098085d2bc5
Michele Angrisano <darksun@zerstorer.local>
2019-04-23 17:07:01 +0200

    Project Euler

--- euler52.py
+++ euler52.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 #It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
-
 #Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+#
+# Answer: 142857 
 
 def problem(limit=1000000):
     for i in range(1000, limit):
